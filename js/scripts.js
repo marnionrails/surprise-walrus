5160981827a970646e33a28175295181dfd36dd5
marnionrails <37088724+marnionrails@users.noreply.github.com>
2021-01-24 15:40:03 -0500

    add horse and walrus, make them show and hide with jquery
    co-authored-by Jay Carr <jaycarr@me.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,8 @@
 
-jQuery(document).ready(function() {
-  jQuery("h1").click(function() {
-    alert("This is a header.")
-  });
-
-  jQuery("p").click(function() {
-    alert("This is a paragraph.");
-  });
-
-  jQuery("img").click(function() {
-    alert("AHHH! A SCARY WALRUS!");
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $(".initially-showing").toggle();
+    $(".initially-hidden").toggle();
   });
 });
 
